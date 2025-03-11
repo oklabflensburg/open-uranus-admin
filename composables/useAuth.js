fc38288594b6f49f95637421d0f9ed40a622e821
Aurelius Wendelken <webtobesocial@gmail.com>
2025-03-11 13:19:25 +0100

    handle token expiration by either refresh token

--- composables/useAuth.js
+++ composables/useAuth.js
@@ -34,9 +34,38 @@ export const useAuth = () => {
     }
   }
 
-  const logout = () => {
-    accessToken.value = null // Remove JWT
+  const refreshAccessToken = async () => {
+    const url = 'https://api.uranus.oklabflensburg.de/user/refresh'
+
+    const formData = new URLSearchParams()
+    formData.append('grant_type', 'refresh_token')
+    formData.append('refresh_token', refreshToken.value)
+    formData.append('client_id', 'string') // Remove if not needed
+    formData.append('client_secret', 'string')
+
+    try {
+      const response = await $fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: formData
+      })
+
+      if (response.access_token) {
+        accessToken.value = response.access_token
+        refreshToken.value = response.refresh_token
+        return true
+      }
+    } catch (error) {
+      console.error('Token refresh failed:', error)
+      logout()
+      return false
+    }
   }
 
-  return { accessToken, refreshToken, login, logout }
+  const logout = () => {
+    accessToken.value = null
+    refreshToken.value = null
+  }
+
+  return { accessToken, refreshToken, login, refreshAccessToken, logout }
 }
