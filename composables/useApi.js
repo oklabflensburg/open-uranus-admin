045072f7f49a84706a97fd45be706d8d3ff56866
Aurelius Wendelken <webtobesocial@gmail.com>
2025-03-11 13:23:06 +0100

    handle token expiration by either refresh token

--- composables/useApi.js
+++ composables/useApi.js
@@ -1,22 +1,46 @@
 import { useAuth } from '@/composables/useAuth'
 
 export const useApi = () => {
-  const { accessToken, refreshAccessToken } = useAuth()
+  const { accessToken, refreshAccessToken, logout } = useAuth()
 
   const fetchApi = async (url, options = {}) => {
     if (!accessToken.value) {
       const refreshed = await refreshAccessToken()
 
-      if (!refreshed) throw new Error('Not authenticated')
+      if (!refreshed) {
+        logout()
+        throw new Error('Not authenticated')
+      }
     }
 
-    return await $fetch(url, {
-      ...options,
-      headers: {
-        Authorization: `Bearer ${accessToken.value}`,
-        ...options.headers
+    try {
+      return await $fetch(url, {
+        ...options,
+        headers: {
+          Authorization: `Bearer ${accessToken.value}`,
+          ...options.headers
+        }
+      })
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        const refreshed = await refreshAccessToken()
+
+        if (!refreshed) {
+          logout()
+          throw new Error('Not authenticated')
+        }
+
+        return await $fetch(url, {
+          ...options,
+          headers: {
+            Authorization: `Bearer ${accessToken.value}`,
+            ...options.headers
+          }
+        })
+      } else {
+        throw error
       }
-    })
+    }
   }
 
   return { fetchApi }
